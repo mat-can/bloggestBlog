f975b822025465b06e88ea2bb9adc0fe3c1a2162
mat-can <matiascandiapy95@gmail.com>
2022-06-06 21:52:40 +0200

    First blog commit

---
+++ Controllers/CoolStuffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoolestMVCProjectEver.Controllers
+{
+    public class CoolStuffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
